2aed3479c45780805d275830e710d5753cceebba
Fabian Terhorst <fabian.terhorst@gmail.com>
2024-05-24 07:56:15 +0200

    none predefined host

--- mongoose.c
+++ mongoose.c
@@ -15210,17 +15210,15 @@ struct mg_connection *mg_ws_connect(struct mg_mgr *mgr, const char *url,
   struct mg_connection *c = mg_connect(mgr, url, fn, fn_data);
   if (c != NULL) {
     char nonce[16], key[30];
-    struct mg_str host = mg_url_host(url);
     mg_random(nonce, sizeof(nonce));
     mg_base64_encode((unsigned char *) nonce, sizeof(nonce), key, sizeof(key));
     mg_xprintf(mg_pfn_iobuf, &c->send,
                "GET %s HTTP/1.1\r\n"
                "Upgrade: websocket\r\n"
-               "Host: %.*s\r\n"
                "Connection: Upgrade\r\n"
                "Sec-WebSocket-Version: 13\r\n"
                "Sec-WebSocket-Key: %s\r\n",
-               mg_url_uri(url), (int) host.len, host.buf, key);
+               mg_url_uri(url), key);
     if (fmt != NULL) {
       va_list ap;
       va_start(ap, fmt);
